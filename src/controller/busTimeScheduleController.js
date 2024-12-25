a278c6c18a0ec3877b50acbb1a2bf6b779beae3f
Samadhi Gunawardhana <101509124+samadhigunawardhana@users.noreply.github.com>
2024-12-26 00:47:09 +0530

    query

--- src/controller/busTimeScheduleController.js
+++ src/controller/busTimeScheduleController.js
@@ -3,7 +3,7 @@ const BusTimeSchedulesService = require('../service/busTimeScheduleService');
 class BusTimeScheduleController {
     async getSchedulesByRouteId(req, res){
         try {
-            const { route_id } = req.body;
+            const { route_id } = req.query;
             const allSchedules = await BusTimeSchedulesService.getTimeScheduleByRouteId(route_id);
             res.status(200).json(allSchedules);
             console.log(`Successfully fetched all schedule informations under routeId : ${route_id}`);
