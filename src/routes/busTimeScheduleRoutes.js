4037d1b53f707c53d07b19d11d4e5efff279d10e
Samadhi Gunawardhana <101509124+samadhigunawardhana@users.noreply.github.com>
2024-12-26 21:43:09 +0530

    post

--- src/routes/busTimeScheduleRoutes.js
+++ src/routes/busTimeScheduleRoutes.js
@@ -29,7 +29,7 @@ router.get('/FindSchedules/:route_id', BusTimeSchedulesController.getSchedulesBy
 /**
  * @swagger
  * /schedules/filterSchedules:
- *   get:
+ *   post:
  *     summary: Filter schedules
  *     description: Filter bus schedules by arrival time and destination.
  *     requestBody:
@@ -84,6 +84,6 @@ router.get('/FindSchedules/:route_id', BusTimeSchedulesController.getSchedulesBy
  *       500:
  *         description: Server error.
  */
-router.get('/filterSchedules', BusTimeSchedulesController.filterSchedulesByArrivalTimeAndDestination);
+router.post('/filterSchedules', BusTimeSchedulesController.filterSchedulesByArrivalTimeAndDestination);
 
 module.exports = router;
