562f844eae59f653f86b2246bea9d7cb213c5b40
Samadhi Gunawardhana <101509124+samadhigunawardhana@users.noreply.github.com>
2024-12-29 13:22:30 +0530

    update app.js

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const swaggerConfig = require('./src/swagger/swaggerConfig');
 const passengerRoutes = require('./src/routes/passengerRoutes');
 const routeRoutes = require('./src/routes/RouteRoutes');
 const busTimeschedule = require('./src/routes/busTimeScheduleRoutes')
+const busBooking = require('./src/routes/busBookingRoutes')
 
 app.use(express.json());
 app.use('/api-check',swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use('/passenger',passengerRoutes);
 app.use('/bus-routes',routeRoutes);
 app.use('/schedules',busTimeschedule);
+app.use('/booking',busBooking)
 app.use
 
 
