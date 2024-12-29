63999fdcbdaf8de91479026064059f9023e7c764
Samadhi Gunawardhana <101509124+samadhigunawardhana@users.noreply.github.com>
2024-12-29 13:34:14 +0530

    up

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api-check',swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use('/passenger',passengerRoutes);
 app.use('/bus-routes',routeRoutes);
 app.use('/schedules',busTimeschedule);
-app.use('/booking',busBooking)
+app.use('/booking',busBooking);
 app.use
 
 
